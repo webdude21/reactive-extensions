91363c8d9a7fe2b9c8591d5f10592a11bf978e22
webdude <dimo.petrof@gmail.com>
2017-05-23 19:36:27 +0300

    added fibonacci generator

---
+++ src/main/java/eu/webdude/rxjava/FibGenerator.java
@@ -0,0 +1,37 @@
+package eu.webdude.rxjava;
+
+import java.math.BigInteger;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.function.Consumer;
+
+
+public class FibGenerator implements Iterable<BigInteger> {
+    @Override
+    public Iterator<BigInteger> iterator() {
+        return new Iterator<BigInteger>() {
+            private BigInteger current = BigInteger.ONE;
+            private BigInteger previous = BigInteger.ZERO;
+
+            @Override
+            public boolean hasNext() {
+                return true;
+            }
+
+            @Override
+            public BigInteger next() {
+                BigInteger temp = current;
+                current = previous.add(current);
+                previous = temp;
+                return temp;
+            }
+        };
+    }
+
+    @Override
+    public void forEach(Consumer<? super BigInteger> action) {
+        for (BigInteger i : this) {
+            action.accept(i);
+        }
+    }
+}
--- src/main/java/eu/webdude/rxjava/Main.java
+++ src/main/java/eu/webdude/rxjava/Main.java
@@ -2,6 +2,7 @@ package eu.webdude.rxjava;
 
 import io.reactivex.Observable;
 
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class Main {
@@ -11,5 +12,12 @@ public class Main {
 				.filter(x -> x % 2 == 0);
 
 		observable.subscribe(System.out::println, x -> System.out.println("Error"), () -> System.out.println("No more values"));
+
+		Observable<BigInteger> fibObservable = Observable.fromIterable(new FibGenerator());
+
+		fibObservable
+				.skipWhile(x -> x.compareTo(BigInteger.valueOf(100_000_000)) < 0)
+				.take(10)
+				.subscribe(System.out::println);
 	}
 }
